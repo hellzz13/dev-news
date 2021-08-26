e3fdae4391969d04cff4cda0c62ae623204fc8e1
hellzz13 <hpalenca13@gmail.com>
2021-08-26 14:25:16 -0300

    client side and severside rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,49 @@
-import styles from '../styles/home.module.scss';
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react"
 
-const Home = () => {
+interface Post{
+  id:string;
+  title: string;
+}
+
+interface HomeProps{
+  posts: Post[]
+}
+
+const Home = ( {posts}: HomeProps ) => {
+  
+  // const [posts, setPosts] = useState<Post[]>([]);
+
+  // useEffect(()=>{
+  //   fetch('http://localhost:3333/posts').then((response)=>{
+  //     response.json().then((data)=> {
+  //       setPosts(data);
+  //     })
+  //   })
+  // },[]);
+
+ 
+
   return (
-    <h1>AGORA <span>VAI!</span></h1>
-  )
+    <>
+    <h1>Posts</h1>
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+        ))}
+    </ul>
+    </>
+  );
 }
 
 export default Home
+
+
+export const getStaticProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+  return {
+    props: {posts,}, 
+  }
+}
  
